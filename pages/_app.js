30542e23cd54d958c6486ca327b8d35d7fdf0741
Liem Tran <liem.tran@opswat.com>
2023-09-03 07:23:50 +0700

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,23 @@
-import '../faust.config';
-import React from 'react';
-import { useRouter } from 'next/router';
-import { FaustProvider } from '@faustwp/core';
-import '@faustwp/core/dist/css/toolbar.css';
-import '../styles/global.scss';
+import "../faust.config";
+import React from "react";
+import { useRouter } from "next/router";
+import { FaustProvider } from "@faustwp/core";
+import { WordPressBlocksProvider } from "@faustwp/blocks";
+import "@faustwp/core/dist/css/toolbar.css";
+import "../styles/global.scss";
 
 export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <FaustProvider pageProps={pageProps}>
-      <Component {...pageProps} key={router.asPath} />
-    </FaustProvider>
-  );
+    return (
+        <FaustProvider pageProps={pageProps}>
+            <WordPressBlocksProvider
+                config={{
+                    blocks,
+                }}
+            >
+                <Component {...pageProps} key={router.asPath} />
+            </WordPressBlocksProvider>
+        </FaustProvider>
+    );
 }
